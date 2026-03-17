6aa687f770803f9b78dcdd8f7afae7a508dcd6a9
esemudeo <info@esemudeo.com>
2026-03-17 11:58:46 +0100

    replace wrong validation annotation

--- src/main/java/nrw/heilmann/quarkus/bot/listeners/GuildReadyListener.java
+++ src/main/java/nrw/heilmann/quarkus/bot/listeners/GuildReadyListener.java
@@ -1,11 +1,11 @@
 package nrw.heilmann.quarkus.bot.listeners;
 
+import jakarta.annotation.Nonnull;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import nrw.heilmann.quarkus.bot.persistence.PenaltyType;
-import org.jspecify.annotations.NonNull;
 
 import java.util.Optional;
 
@@ -14,11 +14,11 @@ public class GuildReadyListener extends ListenerAdapter {
 
 	@Override
 	@Transactional
-	public void onGuildReady(@NonNull GuildReadyEvent event) {
+	public void onGuildReady(@Nonnull GuildReadyEvent event) {
 		createTeamkillPenaltyAsDefault(event);
 	}
 
-	private static void createTeamkillPenaltyAsDefault(@NonNull GuildReadyEvent event) {
+	private static void createTeamkillPenaltyAsDefault(@Nonnull GuildReadyEvent event) {
 		long guildId = event.getGuild().getIdLong();
 		Optional<PenaltyType> teamkillPenaltyType =
 				PenaltyType.<PenaltyType>find("guildId = ?1 and technicalName = ?2", guildId, "teamkill").stream().findAny();
